aaf6e334d4307028b5cb1729ffd18ee233fbe910
Cfcool100 <cfcool100@gmail.com>
2023-09-18 23:59:09 +0000

    custom card

--- lib/components/card.dart
+++ lib/components/card.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class CardRefactor extends StatelessWidget {
-  CardRefactor(this.customColor, {this.child});
+  CardRefactor({
+    required this.customColor,
+    this.child,
+  });
 
   final Color customColor;
   final child;
@@ -14,12 +17,7 @@ class CardRefactor extends StatelessWidget {
         color: customColor,
       ),
       margin: EdgeInsets.all(15.0),
-      height: double.infinity,
       child: child,
-      // child: Icon(
-      //   Icons.female,
-      //   size: 100.0,
-      // ),
     );
   }
 }
