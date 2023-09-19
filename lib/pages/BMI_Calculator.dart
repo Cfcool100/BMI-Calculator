e929ef17759faf47e1069bc10b5afadc743aacf2
Cfcool100 <cfcool100@gmail.com>
2023-09-19 22:34:40 +0000

    brainCalculator adding

--- lib/pages/BMI_Calculator.dart
+++ lib/pages/BMI_Calculator.dart
@@ -1,14 +1,12 @@
+import 'package:bmi_calculator/components/calculator.dart';
 import 'package:bmi_calculator/components/custom_button.dart';
+import 'package:bmi_calculator/pages/result_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
 import '../components/card.dart';
 import '../components/gender_card.dart';
-
-const kValueStyle = TextStyle(
-  fontSize: 45.0,
-  fontWeight: FontWeight.w900,
-);
+import '../components/route_button.dart';
+import '../components/constant_variable.dart';
 
 class BMICalculator extends StatelessWidget {
   @override
@@ -27,19 +25,14 @@ class InputPage extends StatefulWidget {
   _InputPageState createState() => _InputPageState();
 }
 
-enum Gender {
-  male,
-  female,
-}
+enum Gender { male, female }
+
+Gender? gender;
+double _height = 125.0;
+int weight = 55;
+int age = 1;
 
 class _InputPageState extends State<InputPage> {
-  Color primaryBackgroundColor = Color(0XFF1D1E33);
-  Color secondBackgroundColor = Color(0XFF1D1E23);
-  Gender? gender;
-  double height = 125.0;
-  int weight = 55;
-  int age = 1;
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -51,8 +44,8 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: CardRefactor(
                     customColor: gender == Gender.male
-                        ? secondBackgroundColor
-                        : primaryBackgroundColor,
+                        ? kSecondBackgroundColor
+                        : kPrimaryBackgroundColor,
                     child: GenderCard(
                       onPressed: () {
                         setState(() {
@@ -67,8 +60,8 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: CardRefactor(
                     customColor: gender == Gender.female
-                        ? secondBackgroundColor
-                        : primaryBackgroundColor,
+                        ? kSecondBackgroundColor
+                        : kPrimaryBackgroundColor,
                     child: GenderCard(
                       onPressed: () {
                         setState(() {
@@ -85,48 +78,39 @@ class _InputPageState extends State<InputPage> {
           ),
           Expanded(
             child: CardRefactor(
-              customColor: primaryBackgroundColor,
+              customColor: kPrimaryBackgroundColor,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    'HEIGHT',
-                    style: kTextStyle,
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
+                  Text('HEIGHT', style: kTextStyle),
+                  SizedBox(height: 10.0),
                   RichText(
                       text: TextSpan(
                     children: <TextSpan>[
                       TextSpan(
-                        text: height.round().toString(),
-                        style: kValueStyle,
-                      ),
+                          text: _height.round().toString(), style: kValueStyle),
                       TextSpan(
                         text: 'cm',
                         style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.w500,
-                        ),
+                            fontSize: 18.0, fontWeight: FontWeight.w500),
                       ),
                     ],
                   )),
                   Slider(
-                    activeColor: Color(0xffffffff),
-                    overlayColor: MaterialStatePropertyAll(Color(0x39eb1555)),
-                    thumbColor: Color(0xffeb1555),
-                    inactiveColor: Color(0xff8d8e98),
-                    value: height,
+                    activeColor: kSliderActiveColor,
+                    overlayColor: MaterialStatePropertyAll(kSliderOverlayColor),
+                    thumbColor: kSliderThumbColor,
+                    inactiveColor: kSliderInactiveColor,
+                    value: _height,
                     max: 225.0,
                     min: 100.0,
                     divisions: 125,
-                    onChanged: (newValue) {
+                    onChanged: (value) {
                       setState(() {
-                        height = newValue;
+                        _height = value;
                       });
                     },
-                  )
+                  ),
                 ],
               ),
             ),
@@ -136,32 +120,16 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: CardRefactor(
-                    customColor: primaryBackgroundColor,
+                    customColor: kPrimaryBackgroundColor,
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text(
-                          'WEIGHT',
-                          style: kTextStyle,
-                        ),
-                        Text(
-                          '$weight',
-                          style: kValueStyle,
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
+                        Text('WEIGHT', style: kTextStyle),
+                        Text('$weight', style: kValueStyle),
+                        SizedBox(height: 10.0),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            CustomButton(
-                              onPressed: () {
-                                setState(() {
-                                  weight++;
-                                });
-                              },
-                              icon: FontAwesomeIcons.plus,
-                            ),
                             CustomButton(
                               onPressed: () {
                                 setState(() {
@@ -169,6 +137,14 @@ class _InputPageState extends State<InputPage> {
                                 });
                               },
                               icon: FontAwesomeIcons.minus,
+                            ),
+                            CustomButton(
+                              onPressed: () {
+                                setState(() {
+                                  weight++;
+                                });
+                              },
+                              icon: FontAwesomeIcons.plus,
                             ),
                           ],
                         ),
@@ -178,29 +154,15 @@ class _InputPageState extends State<InputPage> {
                 ),
                 Expanded(
                   child: CardRefactor(
-                    customColor: primaryBackgroundColor,
+                    customColor: kPrimaryBackgroundColor,
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text(
-                          'Age',
-                          style: kTextStyle,
-                        ),
-                        Text(
-                          '$age',
-                          style: kValueStyle,
-                        ),
+                        Text('Age', style: kTextStyle),
+                        Text('$age', style: kValueStyle),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            CustomButton(
-                              onPressed: () {
-                                setState(() {
-                                  age++;
-                                });
-                              },
-                              icon: FontAwesomeIcons.plus,
-                            ),
                             CustomButton(
                               onPressed: () {
                                 setState(() {
@@ -208,6 +170,14 @@ class _InputPageState extends State<InputPage> {
                                 });
                               },
                               icon: FontAwesomeIcons.minus,
+                            ),
+                            CustomButton(
+                              onPressed: () {
+                                setState(() {
+                                  age++;
+                                });
+                              },
+                              icon: FontAwesomeIcons.plus,
                             ),
                           ],
                         ),
@@ -218,21 +188,22 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          Container(
-            decoration: BoxDecoration(
-                color: Colors.pinkAccent,
-                borderRadius: BorderRadius.circular(8.0)),
-            margin: EdgeInsets.symmetric(horizontal: 15.0),
-            width: double.infinity,
-            height: 70.0,
-            child: TextButton(
-              onPressed: () {},
-              child: Text(
-                'CALCULATE',
-                style: TextStyle(fontSize: 20.0, color: Colors.white),
-              ),
-            ),
-          )
+          RefactorButton(
+            onClick: () {
+              CalculatorBrain calculate = CalculatorBrain(_height, weight);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ResultPage(
+                    result: calculate.calculateBMI(),
+                    resultMessage: calculate.getResult(),
+                    interpretation: calculate.getResultInterpretation(),
+                  ),
+                ),
+              );
+            },
+            text: 'CALCULATE',
+          ),
         ],
       ),
     );
