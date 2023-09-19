132f3b7445439f365e8b82368f9cb679081d1fc8
Cfcool100 <cfcool100@gmail.com>
2023-09-19 22:41:26 +0000

    constant declare

---
+++ lib/components/constant_variable.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kValueStyle = TextStyle(
+  fontSize: 45.0,
+  fontWeight: FontWeight.w900,
+);
+const kPrimaryBackgroundColor = Color(0XFF1D1E33);
+const kSecondBackgroundColor = Color(0XFF1D1E23);
+const kSliderActiveColor = Color(0xffffffff);
+const kSliderInactiveColor = Color(0xff8d8e98);
+const kSliderOverlayColor = Color(0x39eb1555);
+const kSliderThumbColor = Color(0xffeb1555);
